872926f1ce8cefdd28163306bce19e288caa17e4
Alla <n4lla@ya.ru>
2026-08-01 01:00:50 +0300

    ДЗ 11.14

--- js/form-upload.js
+++ js/form-upload.js
@@ -1,3 +1,4 @@
+import { sendFormData } from './api.js';
 import { resetEffects } from './effects/effects.js';
 import { resetScale } from './scale.js';
 import { isValid, resetValidation } from './validation.js';
@@ -6,6 +7,7 @@ const form = document.querySelector('.img-upload__form');
 const uploadInput = document.querySelector('.img-upload__input');
 const uploadOverlay = document.querySelector('.img-upload__overlay');
 const uploadCancelButton = document.querySelector('.img-upload__cancel');
+const submitButton = form.querySelector('.img-upload__submit');
 
 const hashtagInput = form.querySelector('.text__hashtags');
 const commentInput = form.querySelector('.text__description');
@@ -55,15 +57,89 @@ const onCloseButtonClick = (evt) => {
   closeUploadForm();
 };
 
+const showSuccessMessage = () => {
+  const successTemplate = document.querySelector('#success').content.querySelector('.success');
+  const successElement = successTemplate.cloneNode(true);
+  document.body.append(successElement);
+
+  function onSuccessClose() {
+    successElement.remove();
+    document.removeEventListener('click', onSuccessDocumentClick);
+    document.removeEventListener('keydown', onSuccessDocumentKeydown);
+  }
+
+  function onSuccessDocumentKeydown(evt) {
+    if (evt.key === 'Escape') {
+      onSuccessClose();
+    }
+  }
+
+  function onSuccessDocumentClick(evt) {
+    if (!successElement.contains(evt.target)) {
+      onSuccessClose();
+    }
+  }
+
+  successElement.querySelector('.success__button').addEventListener('click', onSuccessClose);
+  document.addEventListener('click', onSuccessDocumentClick);
+  document.addEventListener('keydown', onSuccessDocumentKeydown);
+};
+
+const showErrorMessage = () => {
+  const errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  const errorElement = errorTemplate.cloneNode(true);
+  document.body.append(errorElement);
+
+  function onErrorClose() {
+    errorElement.remove();
+    document.removeEventListener('click', onErrorDocumentClick);
+    document.removeEventListener('keydown', onErrorDocumentKeydown);
+  }
+
+  function onErrorDocumentKeydown(evt) {
+    if (evt.key === 'Escape') {
+      onErrorClose();
+    }
+  }
+
+  function onErrorDocumentClick(evt) {
+    if (!errorElement.contains(evt.target)) {
+      onErrorClose();
+    }
+  }
+
+  errorElement.querySelector('.error__button').addEventListener('click', onErrorClose);
+  document.addEventListener('click', onErrorDocumentClick);
+  document.addEventListener('keydown', onErrorDocumentKeydown);
+};
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  if (!isValid()) {
+    return;
+  }
+
+  submitButton.disabled = true;
+
+  const formData = new FormData(form);
+
+  sendFormData(formData)
+    .then(() => {
+      closeUploadForm();
+      showSuccessMessage();
+    })
+    .catch(() => {
+      showErrorMessage();
+    })
+    .finally(() => {
+      submitButton.disabled = false;
+    });
+});
+
 const initUploadForm = () => {
   uploadInput.addEventListener('change', onUploadInputChange);
   uploadCancelButton.addEventListener('click', onCloseButtonClick);
 };
-
-form.addEventListener('submit', (evt) => {
-  if(!isValid()){
-    evt.preventDefault();
-  }
-});
 
 export { initUploadForm };
--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-import './util.js';
-import { similarArray } from './data.js';
+import { getPhotos } from './api.js';
 import { renderCards } from './render-cards.js';
 import { initUploadForm } from './form-upload.js';
 
-const photos = similarArray();
-renderCards(photos);
+const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
+
+const showDataError = () => {
+  const dataErrorElement = dataErrorTemplate.cloneNode(true);
+  document.body.append(dataErrorElement);
+
+  setTimeout(() => {
+    dataErrorElement.remove();
+  }, 5000);
+};
+
+getPhotos()
+  .then((photos) => {
+    renderCards(photos);
+  })
+  .catch(() => {
+    showDataError();
+  });
 
 initUploadForm();
 
