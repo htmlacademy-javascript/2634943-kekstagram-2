3d6ce69b576a9c1b6e4adfbb9c104b507f187aa8
Alla <n4lla@ya.ru>
2026-08-07 23:41:00 +0300

    module12-task2

---
+++ js/preview.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('.img-upload__form');
+const previewImage = form.querySelector('.img-upload__preview img');
+const effectsPreviews = form.querySelectorAll('.effects__preview');
+
+const DEFAULT_IMAGE_URL = 'img/upload-default-image.jpg';
+
+let currentPreviewUrl = null;
+
+const setPreviewImage = (file) => {
+  if (currentPreviewUrl) {
+    URL.revokeObjectURL(currentPreviewUrl);
+  }
+
+  currentPreviewUrl = URL.createObjectURL(file);
+  previewImage.src = currentPreviewUrl;
+
+  effectsPreviews.forEach((preview) => {
+    preview.style.backgroundImage = `url(${currentPreviewUrl})`;
+  });
+};
+
+const resetPreviewImage = () => {
+  if (currentPreviewUrl) {
+    URL.revokeObjectURL(currentPreviewUrl);
+    currentPreviewUrl = null;
+  }
+
+  previewImage.src = DEFAULT_IMAGE_URL;
+
+  effectsPreviews.forEach((preview) => {
+    preview.style.backgroundImage = `url(${DEFAULT_IMAGE_URL})`;
+  });
+};
+
+export { setPreviewImage, resetPreviewImage };
